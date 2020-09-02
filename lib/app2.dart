38add44a098f24134a42d40d480eff8bf2908f2f
Glauber Modolo Cabral <glauber.sp@gmail.com>
2020-09-02 11:42:10 -0300

    Alterado para utilizar flutter_modular
    Os apps são módulos filhos em branches separadas e o host é o MainModule.

--- lib/app2.dart
+++ lib/app2.dart
@@ -1,1 +1,16 @@
+import 'package:app2/src/secondPageWidget.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
 export 'src/secondPageWidget.dart';
+
+class App2Module extends ChildModule {
+  static Inject get to => Inject<App2Module>.of();
+
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRouter> get routers => [
+        ModularRouter('/', child: (_, args) => SecondPageWidget()),
+      ];
+}
